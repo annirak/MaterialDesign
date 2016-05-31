d420b32680133bc9116316cbd7cbd0e00de3f72e
annirak <anakarina_987@hotmail.com>
2016-05-31 10:38:12 -0400

    remove fill atribute

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var svgstore = require('gulp-svgstore');
 var svgmin = require('gulp-svgmin');
 var path = require('path');
 var rename = require("gulp-rename");
+var cheerio = require('gulp-cheerio');
 
 var icons = ['printer', 'close', 'lock', 'account', 'email', 'phone', 'arrow-right', 'checkbox-marked-circle',
     'alert-circle', 'dots-vertical', 'plus', 'check', 'library', 'tablet-android', 'account-box-outline', 'water-pump',
@@ -46,17 +47,23 @@ gulp.task('svgstore', function () {
 
     return gulp
         .src(srcFiles)
-        .pipe(svgmin(function (file) {
-            var prefix = path.basename(file.relative, path.extname(file.relative));
-            return {
-                plugins: [{
-                    cleanupIDs: {
-                        prefix: prefix + '-',
-                        minify: true
-                    }
-                }]
-            }
+        .pipe(cheerio({
+            run: function ($) {
+                $('[fill]').removeAttr('fill');
+            },
+            parserOptions: { xmlMode: true }
         }))
+        // .pipe(svgmin(function (file) {
+        //     var prefix = path.basename(file.relative, path.extname(file.relative));
+        //     return {
+        //         plugins: [{
+        //             cleanupIDs: {
+        //                 prefix: prefix + '-',
+        //                 minify: true
+        //             }
+        //         }]
+        //     }
+        // }))
         .pipe(svgstore({ inlineSvg: true }))
         .pipe(rename({
             basename: "mdi",
