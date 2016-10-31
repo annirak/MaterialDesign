5a2adb1fa014c196982eff54a8d0fc0789f8ba11
annirak <anakarina_987@hotmail.com>
2016-10-31 16:43:21 -0400

    Added icons

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,11 @@ var path = require('path');
 var rename = require("gulp-rename");
 var cheerio = require('gulp-cheerio');
 
-var icons = ['printer', 'close', 'lock', 'account', 'email', 'phone', 'arrow-right', 'checkbox-marked-circle',
-    'alert-circle', 'dots-vertical', 'plus', 'check', 'library', 'tablet-android', 'account-box-outline', 'water-pump',
-    'swap-horizontal', 'clipboard-account', 'database', 'settings', 'export', 'clipboard-text', 'book', 'tree', 'chart-line',
-    'book-multiple', 'web', 'chart-timeline', 'more'];
+var icons = ['account', 'account-box-outline', 'account-circle', 'account-search', 'alert-circle', 'arrow-right',
+    'book', 'book-multiple', 'chart-line', 'chart-pie', 'chart-timeline', 'check', 'check-circle', 'checkbox-marked-circle',
+    'clipboard-account', 'clipboard-text', 'close', 'close-circle', 'content-save', 'database', 'delete', 'details', 'document', 'dots-vertical', 'download',
+    'email', 'export', 'information', 'library', 'lock', 'logout', 'magnify', 'more', 'pencil', 'phone', 'plus', 'printer',
+    'settings', 'speedometer', 'swap-horizontal', 'table', 'table-edit', 'tablet-android', 'tree', 'water-pump', 'web'];
 
 var svgFolder = 'icons/svg/';
 
@@ -44,31 +45,51 @@ gulp.task('svgstore', function () {
     icons.forEach(function (icon) {
         srcFiles.push(iconSrc(icon));
     }, this);
-
+    //var dest = 'test/dest';
+    //var dest='C:/tfs/Browser CIS/NYCDEP_BrowserCIS/NYC.DEP.BCIS.WebApp';
+    var dest='C:/tfs/DEP/BrowserCIS/Main/NYC.DEP.BrowserCIS/src/NYC.DEP.BrowserCIS.Web/wwwroot/images';
     return gulp
         .src(srcFiles)
+        .pipe(svgstore({ inlineSvg: true }))
         .pipe(cheerio({
             run: function ($) {
                 $('[fill]').removeAttr('fill');
+                // let x = 0;
+                // let y = 0;
+                // $('symbol').each(function () {
+                //     let id = $(this).attr('id');
+                //     let viewBox = "" + x + " " + y + " 24 24";
+                //     $('svg').append('<view id="' + id + '-view" viewBox="' + viewBox + '" />');
+                //     var vb = viewBox.split(" ");
+                //     $('svg').append('<use xlink:href="#' + id + '" width="' + vb[2] + '" height="' + vb[3] + '" x="' + vb[0] + '" y="' + vb[1] + '" style="fill:rgba(0, 0, 0, 0.54);"></use>');
+                //     y += 24;
+                // });
             },
             parserOptions: { xmlMode: true }
         }))
-        // .pipe(svgmin(function (file) {
-        //     var prefix = path.basename(file.relative, path.extname(file.relative));
-        //     return {
-        //         plugins: [{
-        //             cleanupIDs: {
-        //                 prefix: prefix + '-',
-        //                 minify: true
-        //             }
-        //         }]
-        //     }
-        // }))
-        .pipe(svgstore({ inlineSvg: true }))
         .pipe(rename({
             basename: "mdi",
             extname: ".svg"
         }))
-        .pipe(gulp.dest('C:/tfs/Browser CIS/NYCDEP_BrowserCIS/NYC.DEP.BCIS.WebApp'));
-    //.pipe(gulp.dest('test/dest'));
+        .pipe(gulp.dest(dest));
+        //.pipe(gulp.dest('test/dest'));
+        /* .pipe(rename({ basename: "mdiSprite" }))
+        .pipe(cheerio({
+            run: function ($) {
+                //$('[fill]').removeAttr('fill');
+                let x = 0;
+                let y = 0;
+                $('symbol').each(function () {
+                    let id = $(this).attr('id');
+                    let viewBox = "" + x + " " + y + " 24 24";
+                    $('svg').append('<view id="' + id + '" viewBox="' + viewBox + '" />');
+                    var vb = viewBox.split(" ");
+                    $('svg').append('<use xlink:href="mdi.svg#' + id + '" width="' + vb[2] + '" height="' + vb[3] + '" x="' + vb[0] + '" y="' + vb[1] + '" style="fill:rgba(0, 0, 0, 0.54);"></use>');
+                    y += 24;
+                });
+                $('symbol').remove();
+            },
+            parserOptions: { xmlMode: true }
+        }))
+        .pipe(gulp.dest(dest));*/
 });
